af46dffc6ae8c7aba03dfc6fd4ebb3e32e0007bc
bjperengueza1 <bjperengueza@espe.edu.ec>
2024-12-10 08:51:59 -0500

    faltaantes

---
+++ Infrastructure/Data/Migrations/20241206065600_actualizaciones.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class actualizaciones : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "ExposureId",
+                table: "Authors",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Position",
+                table: "Authors",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Authors_ExposureId",
+                table: "Authors",
+                column: "ExposureId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Authors_Exposures_ExposureId",
+                table: "Authors",
+                column: "ExposureId",
+                principalTable: "Exposures",
+                principalColumn: "ExposureId",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Authors_Exposures_ExposureId",
+                table: "Authors");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Authors_ExposureId",
+                table: "Authors");
+
+            migrationBuilder.DropColumn(
+                name: "ExposureId",
+                table: "Authors");
+
+            migrationBuilder.DropColumn(
+                name: "Position",
+                table: "Authors");
+        }
+    }
+}
