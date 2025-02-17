076af02264603221bc94afc669bb83ce95a48856
bjperengueza1 <bjperengueza@espe.edu.ec>
2025-02-17 04:00:39 -0500

    comint

--- Infrastructure/Settings/FileStorageSettings.cs
+++ Infrastructure/Settings/FileStorageSettings.cs
@@ -2,7 +2,7 @@ namespace Infrastructure.Settings;
 
 public class FileStorageSettings
 {
-    public string BasePath { get; set; } = "../Files";
+    public string BasePath { get; set; } = "Files";
     public string PresentationsPath { get; set; } = "Presentations";
     public string ConferencePath { get; set; } = "Conferences";
     public string TemplateCertificatesPath { get; set; } = "TemplateCertificates";
