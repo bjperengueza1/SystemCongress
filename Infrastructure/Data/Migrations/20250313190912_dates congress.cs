5f10cf4ebaccf44452e09541326e3424242b7457
bjperengueza1 <bjperengueza@espe.edu.ec>
2025-03-14 15:50:56 -0500

    databse

---
+++ Infrastructure/Data/Migrations/20250313190912_dates congress.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class datescongress : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "EndDateNotificationApprove",
+                table: "Congresses",
+                type: "datetime(6)",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "EndDateRegistrationAttendee",
+                table: "Congresses",
+                type: "datetime(6)",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "EndDateRegistrationExposure",
+                table: "Congresses",
+                type: "datetime(6)",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "EndDateNotificationApprove",
+                table: "Congresses");
+
+            migrationBuilder.DropColumn(
+                name: "EndDateRegistrationAttendee",
+                table: "Congresses");
+
+            migrationBuilder.DropColumn(
+                name: "EndDateRegistrationExposure",
+                table: "Congresses");
+        }
+    }
+}
